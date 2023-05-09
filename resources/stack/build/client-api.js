425e4c7f3aa58c232791e50702397189ac758dbd
Timothy Phillips <timothy.phillips@azflse.org>
2023-05-09 11:55:30 -0700

    Repaired client API.

--- resources/stack/build/client-api.js
+++ resources/stack/build/client-api.js
@@ -10,9 +10,9 @@ function renderArticle(data, path) {
 	document.title = title;
 
 	// Update the url.
-	const hash = window.location.hash;
-	const location = window.location.pathname;
-	history.pushState(null, null, window.location.origin + path);
+	// const hash = window.location.hash;
+	// const location = window.location.pathname;
+	// history.pushState(null, null, window.location.origin + path);
 
 	// Update the page content.
 	updatePageContent(data);
@@ -21,8 +21,16 @@ function renderArticle(data, path) {
 	createInnerNavStructure(data);
 
 	// If there was an inter-page link, route to it.
-	triggerRescroll(hash, location);
+	// triggerRescroll(hash, location);
 }
+
+/**
+ * Normalize a route path.
+ * @param {string} path - The path to normalize.
+ * @returns A normalized path.
+ */
+const normalizePath = (path) =>
+	path.includes("content") ? `/content${path.split("content")[1]}` : "/";
 
 /**
  * Request a file by path.
@@ -33,22 +41,25 @@ const handleRequestFile = async (path) => {
 	triggerLoadingArticleContent();
 
 	try {
-		const { data } = await axios.get("/api/v1/markdown", {
-			params: { path },
-		});
+		const normalizedPath = normalizePath(path);
+		const data = content[normalizedPath]
+			.replace(/&#39;/g, "'")
+			.replace(/&quot;/g, '"')
+			.replace(/&gt;/g, ">")
+			.replace(/&lt;/g, "<")
+			.replace(/&amp;/g, "&");
 
-		renderArticle(data, path);
-		indicateSelectedNav(path);
+		renderArticle(data, normalizedPath);
+		indicateSelectedNav(normalizedPath);
 	} catch (err) {
 		console.error(err);
 
 		updatePageContent(
-			`<p class="error"><ion-icon name="warning-outline"></ion-icon> ${err.response.data}</p>`
+			`<p class="error"><ion-icon name="warning-outline"></ion-icon> Could not load that file.</p>`
 		);
 	}
 };
 
-let cancelSearch;
 /**
  * Search files for info.
  * @param {Event} e - The input element's event for oninput.
@@ -57,13 +68,13 @@ const handleSearchFiles = async (e) => {
 	try {
 		const { value: query } = e.target;
 
-		cancelSearch && cancelSearch();
-		const { data } = await axios.get(`/api/v1/markdown/search`, {
-			params: { query },
-			cancelToken: new axios.CancelToken((canceler) => {
-				cancelSearch = canceler;
-			}),
-		});
+		const data = searchIndices
+			.filter(
+				({ path, data }) =>
+					path.toLowerCase().includes(query.toLowerCase()) ||
+					data.includes(query.toLowerCase())
+			)
+			.map(({ path }) => path);
 
 		displaySearchResults(data);
 	} catch (err) {
@@ -72,7 +83,7 @@ const handleSearchFiles = async (e) => {
 };
 
 window.onload = () => {
-	if (window.location.pathname === "/")
-		handleRequestFile("/content/README.html");
-	else handleRequestFile(window.location.pathname);
+	const normalizedPath = normalizePath(window.location.pathname);
+	if (normalizedPath === "/") handleRequestFile("/content/README.html");
+	else handleRequestFile(normalizedPath);
 };
