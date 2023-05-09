f1e3b2a501fe46e9192e5485317c50391aef6c93
Timothy Phillips <timothy.phillips@azflse.org>
2023-05-09 10:35:27 -0700

    Implemented basic static app compilation.

--- resources/scripts/build_app.js
+++ resources/scripts/build_app.js
@@ -29,11 +29,11 @@ marked.use(
 
 /**
  * Recursively convert all markdown files to html and build search indices.
- * @param {*} path -  The path to the file.
+ * @param {*} path -  The path to the output directory.
  * @param {string} cwd - The current working directory.
  * @returns Newly generated search indices.
  */
-const compile_file_tree = (path, cwd) => {
+const compile_file_tree = (cwd, path, cwd) => {
 	const files = fs.readdirSync(path);
 
 	let search_indices = [];
@@ -79,6 +79,62 @@ const compile_file_tree = (path, cwd) => {
 	});
 
 	return search_indices;
+};
+
+/**
+ * Recursively convert all markdown files to html and build search indices.
+ * @param {string} cwd - The current working directory.
+ * @param {string} src -  The path to the output file.
+ * @param {string} dest -  The path to the output file.
+ * @returns Newly generated search indices.
+ */
+const compile_static_content = (cwd, src, dest) => {
+	const files = fs.readdirSync(src);
+
+	let search_indices = [];
+	let content = {};
+
+	files.forEach((file) => {
+		const filePath = `${src}/${file}`;
+		const stat = fs.statSync(filePath);
+
+		if (stat.isDirectory()) {
+			const {
+				content: additionalContent,
+				search_indices: additionalSearchIndices,
+			} = compile_static_content(filePath, cwd);
+
+			content = { ...content, ...additionalContent };
+			search_indices = [...search_indices, ...additionalSearchIndices];
+		} else {
+			const existingData = fs.readFileSync(filePath, "utf-8");
+
+			const convertedData = marked.parse(existingData);
+			const newPath = replace_all(filePath, ".md", ".html"); // Replace any .md file extensions with .html.
+
+			content[newPath] = convertedData;
+
+			console.log(`Creating search directory for "${newPath}"`);
+
+			// Store page indeces.
+			search_indices = [
+				...search_indices,
+				{
+					// Replace all instances of the cwd or "/build/server" from the newly normalized path.
+					path: replace_all(
+						replace_all(normalize_path(newPath), cwd, ""),
+						"/build/server",
+						""
+					),
+					data: Array.from(new Set(existingData.split(" ")))
+						.join(" ")
+						.toLowerCase(),
+				},
+			];
+		}
+	});
+
+	return { content, search_indices };
 };
 
 /**
@@ -164,7 +220,7 @@ const copy_html = async (buildResourcesDir, target, config, imports) => {
  */
 const build_server = async (stackDir, target, config, cwd) => {
 	// Copy the build structure.
-	console.log("Copying server.");
+	console.log("Building server.");
 	const buildResourcesDir = `${stackDir}/build`;
 	await copy_paths([
 		[`${buildResourcesDir}/server.js`, `${target}/server.js`, "file"],
@@ -210,7 +266,7 @@ const build_server = async (stackDir, target, config, cwd) => {
 		);
 	}
 
-	console.log("Finished copying server.");
+	console.log("Finished building server.");
 };
 
 /**
@@ -290,7 +346,86 @@ const build_client = async (stackDir, target, config, cwd) => {
  * @param {*} config - The user's configuration object.
  * @param {string} cwd - The current working directory.
  */
-const build_static = async (stackDir, target, config, cwd) => {};
+const build_static = async (stackDir, target, config, cwd) => {
+	// Add sub-directory to target.
+	const contentTarget = `${target}/server/content`;
+
+	// Create the build directories.
+	console.log("Copying necessary client directories.");
+	const directories = ["public"];
+	for (const dir of directories)
+		if (!(await fs.exists(dir))) await fs.mkdir(`${target}/${dir}`);
+
+	// Copy the build files.
+	console.log("Copying necessary client files.");
+	const buildResourcesDir = `${stackDir}/build`;
+	await copy_paths([
+		[`${cwd}/styles`, `${target}/styles`, "directory"],
+		[`${cwd}/public`, `${target}/public`, "directory"],
+		[`${stackDir}/style.css`, `${target}/style.css`, "file"],
+		[`${stackDir}/style.css.map`, `${target}/style.css.map`, "file"],
+		[
+			`${buildResourcesDir}/static-api.js`,
+			`${target}/client-api.js`,
+			"file",
+		],
+		[`${buildResourcesDir}/client.js`, `${target}/client.js`, "file"],
+		[`${buildResourcesDir}/404.html`, `${target}/404.html`, "file"],
+	]);
+
+	// Build app indeces.
+	console.log("Building file structure caches.");
+	const caches = [
+		[
+			// The file tree is localized to not include the servers entire file structure in the path.
+			`const fileTree = ${replace_all(
+				replace_all(
+					JSON.stringify(
+						dirTree(contentTarget, { normalizePath: true })
+					),
+					cwd,
+					""
+				),
+				"/build/server",
+				""
+			)}`,
+			"file-tree.js",
+		],
+	];
+	for (const [data, dest] of caches) {
+		await fs.outputFile(`${target}/${dest}`, data);
+	}
+
+	// Build static content.
+	console.log("Building static client content.");
+
+	// Convert all markdown to pre-rendered html and get search indices.
+	const search_indices = compile_static_content(
+		cwd,
+		`${cwd}/content`,
+		`${target}/content.js`
+	);
+
+	// Save search indices.
+	await fs.outputFile(
+		`${target}/search-indices.js`,
+		`const searchIndices=${JSON.stringify(search_indices)};`,
+		{ recursive: true }
+	);
+
+	// Configure and copy html.
+	await copy_html(buildResourcesDir, `${target}/index.html`, config, [
+		{
+			path: "https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js",
+			type: "cdn",
+		},
+		{ path: "file-tree.js", type: "script" },
+		{ path: "search-indices.js", type: "script" },
+		{ path: "content.js", type: "script" },
+		{ path: "client-api.js", type: "script" },
+		{ path: "client.js", type: "script" },
+	]);
+};
 
 /**
  * Build an extrusive app.
